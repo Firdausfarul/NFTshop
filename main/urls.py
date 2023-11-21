8f8fb2a5de2b40ff7a04a52424acb183f3ac0c59
Muhammad Fachrudin Firdaus <firdausfarul@gmail.com>
2023-11-21 22:28:11 +0700

    update url

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id , register, login_user, logout_user, delete_product, increase_amount, decrease_amount, get_product_json, add_product_ajax, get_product_json_by_user_id
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id , register, login_user, logout_user, delete_product, increase_amount, decrease_amount, get_product_json, add_product_ajax, get_product_json_by_user_id, create_product_flutter
 app_name = 'main'
 
 urlpatterns = [
@@ -18,6 +18,8 @@ urlpatterns = [
     path('increase_amount/<int:id>/', increase_amount, name='increase_amount'),
     path('decrease_amount/<int:id>/', decrease_amount, name='decrease_amount'),
     path('get_product_json/', get_product_json, name='get_product_json'),
-    path('create-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+
 ]
 
